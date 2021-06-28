a9c47a6b88d7bfe8be3152aea0abd241f8f78380
enea5 <eneaa.aliko@gmail.com>
2021-06-28 10:11:41 -0700

    v4.3.3

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ Description: Schemati Settings
 Plugin URI: https://schemamarkapp.com/
 Author: Shay Ohayon
 Author URI: https://schemamarkapp.com/
-Version: 4.3.2
+Version: 4.3.3
 */
 
 require_once __DIR__."/load.php";
